5440f5709b3f321471c1ac3113ac999e33139b53
drippydik <drippydik@gmail.com>
2017-10-29 15:36:49 +0000

    initial

---
+++ lesson1.py
@@ -0,0 +1,32 @@
+#!/data/data/com.termux/files/home/.virtualenvs/ocr/bin/python
+
+# you can ignore the top line it is just to tell the file where to find the python App.
+
+# it is called the She-bang and it is a special type of comment
+
+# a comment is anything after a "#" symbol like this line and all the ones above. the below
+# comments are instructions for you to copy
+
+
+
+#	            _       _   
+#	 _ __  _ __(_)_ __ | |_ 
+#	| '_ \| '__| | '_ \| __|
+#	| |_) | |  | | | | | |_ 
+#	| .__/|_|  |_|_| |_|\__|
+#	|_|                     
+
+# 1. write your name in the next line like print("Bob") remember the quotes
+
+print("tiger")
+
+# after press "esc" ==> ":w" ==> "return"
+
+# this saves your program
+
+# then to test it press "esc" ==> ":!python lesson1.py" ==> press "return"
+
+# did you see your name appear?
+
+# if yes, then well done. if no, then you might have an error try rerunning the program and seeing
+# if you can figure out what is wrong
